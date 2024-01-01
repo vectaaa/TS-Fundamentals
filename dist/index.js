e95e3d7363c01a2725483aac3f0755c4598dc592
vectaaa <victoronuchukwu16@gmail.com>
2024-01-01 20:03:28 +0100

    Typescript Fundamentals

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+let phone = 234812232121;
+let course = 'Maths';
+let is_Finished = true;
+let level;
+let number = [1, 2, 4];
+let user = [1, 'Ready'];
+console.log(user);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let phone = 234_812_232_121;
+let course = 'Maths';
+let is_Finished = true;
+let level;
+
+let number = [1,2,4];
+
+//Tuples
+let user: [number, string] = [1, 'Ready'];
+console.log(user);
+
+//Enums they are used to specify constants in classifications e.g {Sizes of clothes, days of the week, colors, month of the year etc.}
+enum Sizes {Small, Medium, Large};
+let mySize = Sizes.Small;
+console.log(mySize);
